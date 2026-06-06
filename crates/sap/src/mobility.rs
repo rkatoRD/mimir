11ac5a56e60627f2222475d5522b47ed1b7e447c
private <maypul.private@gmail.com>
2026-06-07 00:46:37 +0900

    refact: standardizing naming conventions

--- crates/sap/src/mobility.rs
+++ crates/sap/src/mobility.rs
@@ -2,7 +2,7 @@ use crate::messages::SlotContext;
 use nr_core::{Point, SimRng, UeId};
 
 pub trait MobilityModel {
-    fn next_position(
+    fn step(
         &mut self,
         ctx: &SlotContext,
         ues: &[UeId],
